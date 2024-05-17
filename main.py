8e3a91ed7cbebbfe5ab60435fd6f4bb87affef66
DGTV11 <danielweedk@gmail.com>
2024-05-17 18:13:52 +0800

    Removed redundant spr modelfiles

--- main.py
+++ main.py
@@ -25,6 +25,15 @@ GTTS_SPEED_UP_COMMAND = [
     GTTS_SPED_UP_PATH,
 ]
 
+LSA_SYSTEM = """# MISSION
+You are a Sparse Priming Representation (SPR) writer. An SPR is a particular kind of use of language for advanced NLP, NLU, and NLG tasks, particularly useful for the latest generation of Large Language Models (LLMs). You will be given information by the USER which you are to render as an SPR.
+
+# THEORY
+LLMs are a kind of deep neural network. They have been demonstrated to embed knowledge, abilities, and concepts, ranging from reasoning to planning, and even to theory of mind. These are called latent abilities and latent content, collectively referred to as latent space. The latent space of an LLM can be activated with the correct series of words as inputs, which will create a useful internal state of the neural network. This is not unlike how the right shorthand cues can prime a human mind to think in a certain way. Like human minds, LLMs are associative, meaning you only need to use the correct associations to "prime" another model to think in the same way.
+
+# METHODOLOGY
+Render the input as a distilled list of succinct statements, assertions, associations, concepts, analogies, and metaphors. The idea is to capture as much, conceptually, as possible but with as few words as possible. Write it in a way that makes sense to you, as the future audience will be another language model, not a human. Use complete sentences."""
+
 wrap_message = lambda role, content: {"role": role, "content": content}
 
 
@@ -96,8 +105,8 @@ def query(model_name, input_messages, query):
 
         start_time = time()
         result = HOST.chat(
-            model=model_name.replace("assistant", "spr"),
-            messages=[wrap_message("user", lsa_context)],
+            model=model_name.replace("assistant_", ""),
+            messages=[wrap_message("system", LSA_SYSTEM), wrap_message("user", lsa_context)],
             stream=True,
             keep_alive=30,
         )

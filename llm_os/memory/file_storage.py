fdeb5ba74e0d7d958ebe6dc58d8f45f977836a72
Daniel Wee <danielweedk@gmail.com>
2024-10-09 17:28:12 +0800

    Continued implementing file storage

--- llm_os/memory/file_storage.py
+++ llm_os/memory/file_storage.py
@@ -1,4 +1,4 @@
-from os import path, mkdir
+from os import path, mkdir, remove
 from datetime import datetime
 import json
 import pathlib
@@ -23,11 +23,13 @@ class FileStorage:
         if not path.exists(self.folder_path):
             mkdir(self.folder_path)
 
-    def __len__(self): #TODO
-        #return len(self.get_file_paths())
-        pass
+    def __len__(self):
+        return sum(lambda id: len(self.get_file_paths(id)), self.get_all_user_ids_with_folders)
 
-    def __write_file_summaries(self, user_id, hashes):
+    def get_all_user_ids_with_folders(self):
+        return [f for f in os.listdir(self.folder_path) if os.path.isdir(f)]
+
+    def __write_file_summaries(self, user_id, hashes, file_rel_path_parts=None, edit_mode=None):
         repo_path = self.__get_repo_path_from_user_id(user_id)
         repo = self.__load_repo(repo_path)
 
@@ -36,8 +38,12 @@ class FileStorage:
         with open(summaries_path, "w") as f:
             f.write(json.dumps(hashes))
 
-        repo.index.add([summaries_path])
-        repo.index.commit(f"Updated file_summaries.json for user {user_id}")
+        if not file_rel_path_parts:
+            repo.index.add([summaries_path])
+            repo.index.commit(f"Updated file_summaries.json for user {user_id}")
+        else:
+            repo.index.add([path.join(repo_path, *file_rel_path_parts), summaries_path])
+            repo.index.commit(f"{edit_mode} {path.join(*file_rel_path_parts)} for user {user_id}")
 
     def __read_file_summaries(self, user_id):
         repo_path = self.__get_repo_path_from_user_id(user_id)
@@ -83,16 +89,6 @@ class FileStorage:
         repo_pathlib_dir = pathlib.Path(repo_path)
         return [str(item.relative_to(repo_pathlib_dir)) for item in repo_pathlib_dir.rglob("*") if item.is_file() and item.parts.isdisjoint(BLACKLISTED_FOLDERS_OR_FILES)]
 
-    def make_file(self, user_id, file_rel_path_parts):
-        repo_path = self.__get_repo_path_from_user_id(user_id)
-        repo = self.__load_repo(repo_path)
-
-        new_file_path = path.join(repo_path, *file_rel_path_parts)
-        f = open(new_file_path, "x")
-        f.close()
-
-        #TODO: create placeholder summary here
-
     def get_file_summary(self, user_id, file_rel_path_parts):
         repo_path = self.__get_repo_path_from_user_id(user_id)
         summaries = self.__read_file_summaries(user_id)
@@ -101,8 +97,8 @@ class FileStorage:
         file_path = path.join(repo_path, *file_rel_path_parts)
         file_hash = self.__compute_file_hash(file_path)
 
-        if not (rel_path_parts_tuple in summaries and summaries[rel_path_parts_tuple]['file_hash'] == file_hash):
-            summaries[rel_path_parts_tuple] = {'file_hash': file_hash}
+        if not (file_rel_path_parts_tuple in summaries and summaries[file_rel_path_parts_tuple]['file_hash'] == file_hash):
+            summaries[file_rel_path_parts_tuple] = {'file_hash': file_hash}
             splitter = MarkdownSplitter.from_huggingface_tokenizer(QWEN_2_5_TOKENIZER, 8192)
             summary = ""
 
@@ -110,8 +106,40 @@ class FileStorage:
                 for chunk in splitter.chunks(f.read()):
                     summary += spr_compress('qwen2.5:0.5b', 8192, chunk) + "\n"
 
-            summaries[rel_path_parts_tuple]['summary'] = summary
+            summaries[file_rel_path_parts_tuple]['summary'] = summary
 
             self.__write_file_summaries(user_id, summaries)
 
-        return summaries[rel_path_parts_tuple]['summary']
+        return summaries[file_rel_path_parts_tuple]['summary']
+
+    def make_file(self, user_id, file_rel_path_parts):
+        repo_path = self.__get_repo_path_from_user_id(user_id)
+        repo = self.__load_repo(repo_path)
+
+        new_file_rel_path_parts_tuple = tuple(file_rel_path_parts)
+        new_file_path = path.join(repo_path, *file_rel_path_parts)
+        f = open(new_file_path, "x")
+        f.close()
+
+        with open(new_file_path, "w") as f:
+            f.write('Nothing here (yet)')
+
+        file_hash = self.__compute_file_hash(new_file_path)
+        summaries[new_file_rel_path_parts_tuple] = {'file_hash': file_hash}
+
+        summaries[new_file_rel_path_parts_tuple]['summary'] = 'Empty file'
+
+        self.__write_file_summaries(user_id, summaries, new_file_rel_path_parts, "Created")
+
+    def remove_file(self, user_id, file_rel_path_parts):
+        repo_path = self.__get_repo_path_from_user_id(user_id)
+        repo = self.__load_repo(repo_path)
+
+        file_rel_path_parts_tuple = tuple(file_rel_path_parts)
+        file_path = path.join(repo_path, *file_rel_path_parts)
+
+        remove(file_path)
+
+        del summaries[file_rel_path_parts_tuple]
+
+        self.__write_file_summaries(user_id, summaries, file_rel_path_parts, "Removed")

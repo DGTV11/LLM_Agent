a37b374bf3b2e3b534a53d889a4c2cdc9ac442cf
Daniel Wee <danielweedk@gmail.com>
2025-06-20 12:01:43 +0800

    Migrated from flask to FastAPI (because yes)

--- server.py
+++ server.py
@@ -1,37 +1,56 @@
-from os import path, listdir, mkdir, rmdir
-import threading
-from time import time
-from datetime import timedelta
-from uuid import uuid4
 import json
+from asyncio import Semaphore
+from datetime import timedelta
+from os import listdir, mkdir, path, rmdir
+from time import time
+from uuid import uuid4
 
-from flask import Flask, Response, request, jsonify, stream_with_context
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+from pydantic import BaseModel
 
 from config import CONFIG
-
 from llm_os.agent import Agent
+from llm_os.functions.load_functions import (
+    get_function_dats_from_function_sets,
+    load_all_function_sets,
+)
 from llm_os.interface import CLIInterface, ServerInterface
-from llm_os.web_interface import WebInterface
-from llm_os.tokenisers import get_tokeniser_and_context_window
-
-from llm_os.memory.memory import Memory
-from llm_os.memory.working_context import WorkingContext
-from llm_os.memory.recall_storage import RecallStorage
 from llm_os.memory.archival_storage import ArchivalStorage
 from llm_os.memory.file_storage import FileStorage
-
+from llm_os.memory.memory import Memory
+from llm_os.memory.recall_storage import RecallStorage
+from llm_os.memory.working_context import WorkingContext
 from llm_os.prompts.gpt_system import get_system_text
+from llm_os.tokenisers import get_tokeniser_and_context_window
+from llm_os.web_interface import WebInterface
 
-from llm_os.functions.load_functions import (
-    load_all_function_sets,
-    get_function_dats_from_function_sets,
-)
+app = FastAPI()
 
-app = Flask(__name__)
-
-sem = threading.Semaphore()
+sem = Semaphore(1)
 
 loaded_agents = {}
+
+
+class InitAgentRequestParams(BaseModel):
+    agent_persona_name: str
+    human_persona_name: str
+
+
+class ConvNameGenericRequestParams(BaseModel):
+    conv_name: str
+
+
+class CreateHumanRequestParams(BaseModel):
+    conv_name: str
+    human_persona_name: str
+
+
+class SendMessageParams(BaseModel):
+    conv_name: str
+    user_id: int
+    message: str
 
 
 def get_agent(conv_name):
@@ -160,80 +179,61 @@ def init_agent(agent_persona_name, human_persona_name):
     return conv_name
 
 
-@app.route("/conversation-ids", methods=["GET"])
-def get_existing_conversation_ids():
-    print('FINISHED RUNNING GET "/conversation-ids"')
-    return jsonify(
-        {
-            "conv_ids": list(
-                filter(
-                    lambda s: s[0] != ".",
-                    listdir(path.join(path.dirname(__file__), "persistent_storage")),
-                )
+@app.get("/conversation-ids")
+async def get_existing_conversation_ids():
+    return {
+        "conv_ids": list(
+            filter(
+                lambda s: s[0] != ".",
+                listdir(path.join(path.dirname(__file__), "persistent_storage")),
             )
-        }
-    )
+        )
+    }
 
 
-@app.route("/personas/agents", methods=["GET"])
-def get_agent_personas():
-    print('FINISHED RUNNING GET "/personas/agents"')
-    return jsonify(
-        {
-            "persona_names": listdir(
-                path.join(path.dirname(__file__), "llm_os", "personas", "agents")
-            )
-        }
-    )
+@app.get("/personas/agents")
+async def get_agent_personas():
+    return {
+        "persona_names": listdir(
+            path.join(path.dirname(__file__), "llm_os", "personas", "agents")
+        )
+    }
 
 
-@app.route("/personas/humans", methods=["GET"])
-def get_human_personas():
-    print('FINISHED RUNNING GET "/personas/humans"')
-    return jsonify(
-        {
-            "persona_names": listdir(
-                path.join(path.dirname(__file__), "llm_os", "personas", "humans")
-            )
-        }
-    )
+@app.get("/personas/humans")
+async def get_human_personas():
+    return {
+        "persona_names": listdir(
+            path.join(path.dirname(__file__), "llm_os", "personas", "humans")
+        )
+    }
 
 
-@app.route("/agent", methods=["POST", "DELETE"])
-def agent_methods():
-    match request.method:
-        case "POST":
-            # Load personas
-            data = request.get_json()
-            agent_persona_name = data.get("agent_persona_name")
-            human_persona_name = data.get("human_persona_name")
+@app.post("/agent")
+async def init_agent(data: InitAgentRequestParams):
+    agent_persona_name = data.agent_persona_name
+    human_persona_name = data.human_persona_name
 
-            conv_name = init_agent(agent_persona_name, human_persona_name)
+    conv_name = init_agent(agent_persona_name, human_persona_name)
 
-            print('FINISHED RUNNING POST "/agent"')
-            return jsonify({"conv_name": conv_name})
-        case "DELETE":
-            # Load conversation name
-            data = request.get_json()
-            conv_name = data.get("conv_name")
-
-            # Remove conversation
-            try:
-                rmdir(
-                    path.join(path.dirname(__file__), "persistent_storage", conv_name)
-                )
-                print('FINISHED RUNNING DELETE "/agent"')
-                return jsonify({"success": True})
-            except OSError as error:
-                print('FINISHED RUNNING DELETE "/agent"')
-                return jsonify({"success": False})
+    return {"conv_name": conv_name}
 
 
-@app.route("/agent/humans", methods=["GET", "POST"])
-def agent_human_methods():
-    # Load data
-    data = request.get_json()
-    conv_name = data.get("conv_name")
+@app.delete("/agent")
+async def delete_agent(data: ConvNameGenericRequestParams):
+    conv_name = data.conv_name
+
+    # Remove conversation
+    try:
+        rmdir(path.join(path.dirname(__file__), "persistent_storage", conv_name))
+        return {"success": True}
+    except OSError as error:
+        return {"success": False}
+
+
+@app.get("/agent/humans")
+async def get_all_human_ids(data: ConvNameGenericRequestParams):
+    conv_name = data.conv_name
 
     # Load working context
     working_context = WorkingContext(CONFIG["model_name"], conv_name, None, None, None)
@@ -241,43 +241,46 @@ def agent_human_methods():
     # Get all registered human ids
     human_ids = list(working_context.humans.keys())
 
-    match request.method:
-        case "GET":
-            print('FINISHED RUNNING GET "/agent/humans"')
-            return jsonify({"human_ids": human_ids})
-        case "POST":
-            # Load human persona
-            human_persona_name = data.get("human_persona_name")
-
-            human_persona_fp = path.join(
-                path.dirname(__file__),
-                "llm_os",
-                "personas",
-                "humans",
-                human_persona_name,
-            )
-
-            with open(human_persona_fp, "r") as f:
-                human_persona_str = f.read()
-
-            # Add new user
-            new_human_id = max(human_ids) + 1
-            working_context.add_new_human_persona(new_human_id, human_persona_str)
-
-            print('FINISHED RUNNING POST "/agent/humans"')
-            return jsonify({"new_human_id": new_human_id})
+    return {"human_ids": human_ids}
 
 
-@app.route("/messages/send", methods=["POST"])
-def send_message():
-    sem.acquire()
+@app.post("/agent/humans")
+async def create_human(data: CreateHumanRequestParams):
+    conv_name = data.conv_name
+    human_persona_name = data.human_persona_name
 
-    try:
+    # Load working context
+    working_context = WorkingContext(CONFIG["model_name"], conv_name, None, None, None)
+
+    # Get all registered human ids
+    human_ids = list(working_context.humans.keys())
+
+    # Load human persona
+    human_persona_fp = path.join(
+        path.dirname(__file__),
+        "llm_os",
+        "personas",
+        "humans",
+        human_persona_name,
+    )
+
+    with open(human_persona_fp, "r") as f:
+        human_persona_str = f.read()
+
+    # Add new user
+    new_human_id = max(human_ids) + 1
+    working_context.add_new_human_persona(new_human_id, human_persona_str)
+
+    return {"new_human_id": new_human_id}
+
+
+@app.post("/messages/send")
+async def send_message(data: SendMessageParams):
+    async with sem:
         # Load data
-        data = request.get_json()
-        conv_name = data.get("conv_name")
-        user_id = data.get("user_id")
-        message = data.get("message")
+        conv_name = data.conv_name
+        user_id = data.user_id
+        message = data.message
 
         # Load agent
         agent = get_agent(conv_name)
@@ -293,7 +296,6 @@ def send_message():
         )
 
         # Generate agent responses
-        @stream_with_context
         def generate_agent_responses(agent_obj):
             total_start_time = time()
 
@@ -329,22 +331,16 @@ def send_message():
                 }
             ) + "\n"
 
-        print('FINISHED RUNNING POST "/messages/send"')
-        return Response(generate_agent_responses(agent), mimetype="application/json")
-    finally:
-        sem.release()
+        return StreamingResponse(generate_agent_responses(agent))
 
 
-@app.route("/messages/send/first-message", methods=["POST"])
-def send_first_message():
-    sem.acquire()
-
-    try:
+@app.post("/messages/send/first-message")
+async def send_first_message(data: SendMessageParams):
+    async with sem:
         # Load data
-        data = request.get_json()
-        conv_name = data.get("conv_name")
-        user_id = data.get("user_id")
-        message = data.get("message")
+        conv_name = data.conv_name
+        user_id = data.user_id
+        message = data.message
 
         # Load agent
         agent = get_agent(conv_name)
@@ -360,7 +356,6 @@ def send_first_message():
         )
 
         # Generate agent responses
-        @stream_with_context
         def generate_agent_responses(agent_obj):
             total_start_time = time()
 
@@ -396,22 +391,16 @@ def send_first_message():
                 }
             ) + "\n"
 
-        print('FINISHED RUNNING POST "/messages/send/first-message"')
-        return Response(generate_agent_responses(agent), mimetype="application/json")
-    finally:
-        sem.release()
+        return StreamingResponse(generate_agent_responses(agent))
 
 
-@app.route("/messages/send/no-heartbeat", methods=["POST"])
-def send_message_without_heartbeat():
-    sem.acquire()
-
-    try:
+@app.post("/messages/send/no-heartbeat")
+async def send_message_without_heartbeat(data: SendMessageParams):
+    async with sem:
         # Load data
-        data = request.get_json()
-        conv_name = data.get("conv_name")
-        user_id = data.get("user_id")
-        message = data.get("message")
+        conv_name = data.conv_name
+        user_id = data.user_id
+        message = data.message
 
         # Load agent
         agent = get_agent(conv_name)
@@ -431,21 +420,17 @@ def send_message_without_heartbeat():
         server_message_stack = agent.interface.server_message_stack.copy()
         agent.interface.server_message_stack = []
 
-        print('FINISHED RUNNING POST "/messages/send/no-heartbeat"')
-        return jsonify(
-            {
-                "server_message_stack": server_message_stack,
-                "ctx_info": {
-                    "current_ctx_token_count": agent.memory.main_ctx_message_seq_no_tokens,
-                    "ctx_window": agent.memory.ctx_window,
-                },
-            }
-        )
-    finally:
-        sem.release()
+        return {
+            "server_message_stack": server_message_stack,
+            "ctx_info": {
+                "current_ctx_token_count": agent.memory.main_ctx_message_seq_no_tokens,
+                "ctx_window": agent.memory.ctx_window,
+            },
+        }
 
 
 if __name__ == "__main__":
     if not CONFIG:
         raise ValueError("Config not found, please run config.py")
-    app.run(host="0.0.0.0", port=4999)
+    # app.run(host="0.0.0.0", port=4999)
+    uvicorn.run(app, port=4999, host="0.0.0.0")
